3a5d16ded012390033e1207a609c0caa382ad327
Shohan494 <shohan12224943@gmail.com>
2017-06-04 11:16:42 +0600

    edited some more documantation and upated the readme

--- doc/api_data.js
+++ doc/api_data.js
@@ -5,8 +5,56 @@ define({ "api": [
     "title": "Request User information",
     "name": "getUser",
     "group": "User",
-    "version": "0.0.0",
+    "version": "0.1.1",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>The users name.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Example data on success",
+          "content": "{\n  name: 'Shohan'\n}",
+          "type": "json"
+        }
+      ]
+    },
     "filename": "./index.js",
     "groupTitle": "User"
+  },
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p>"
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "./doc/main.js",
+    "group": "_home_shohan_cactus_apidoc_test_doc_main_js",
+    "groupTitle": "_home_shohan_cactus_apidoc_test_doc_main_js",
+    "name": ""
   }
 ] });
--- index.js
+++ index.js
@@ -6,6 +6,15 @@ var currentUser = {
  * @api {get}/user Request User information
  * @apiName getUser
  * @apiGroup User
+ * @apiVersion 0.1.1
+ *
+ * @apiSuccess {String} name The users name.
+ *
+ * @apiSuccessExample Example data on success
+ * {
+ *   name: 'Shohan'
+ * }
+ *
  */
 function getUser(){
   return {code: 200, data: currentUser}
